6bde0244ee905ab27359a829ec107a8e0f5faa58
Firdevs Çolak <firdevscolak81@gmail.com>
2025-05-05 22:32:31 +0300

    done

--- optimize.py
+++ optimize.py
@@ -11,28 +11,28 @@ df_data2 = pd.read_csv("datasets/melb_data.csv")
 
 filtered_df_data2 = df_data2.dropna(axis=0)
 b = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 'YearBuilt', 'Lattitude', 'Longtitude']
-y2 = filtered_df_data2.Price
-X2 = filtered_df_data2[b]
+y = filtered_df_data2.Price
+X = filtered_df_data2[b]
 
 df_model2 = DecisionTreeRegressor()
-df_model2.fit(X2, y2)
+df_model2.fit(X, y)
 
-predicted_home_prices = df_model2.predict(X2)
-mean_absolute_error(y2, predicted_home_prices)
+predicted_home_prices = df_model2.predict(X)
+mean_absolute_error(y, predicted_home_prices)
 # Out[26]: 434.71594577146544 --> in-sample score
 
-# validation value(just for test,model doesnt see):
+# validation value(just for test,model doesn't see):
 
-train_X2, val_X2, train_y2, val_y2 = train_test_split(X2, y2, random_state=0)
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
 # random_state= 0,1,42 --> it divides same for evey time
 # 42: origin num (The Hitchhiker's Guide to the Galaxy ref.)
 # if none: it divides different for evey time
 
 df_model3 = DecisionTreeRegressor()
-df_model3.fit(train_X2, train_y2)
+df_model3.fit(train_X, train_y)
 
-val2_prediction = df_model3.predict(val_X2)
-print(mean_absolute_error(val_y2, val2_prediction))
+val_prediction = df_model3.predict(val_X)
+print(mean_absolute_error(val_y, val_prediction))
 # out: 260042.310522918
 
 # UNDERFITTING AND OVERFITTING
@@ -47,20 +47,20 @@ in the raw data.
 df_data2 = pd.read_csv("datasets/melb_data.csv")
 a = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
 
-y3 = df_data2.Price  # target variable
-X3 = df_data2[a]  # features matris
+y2 = df_data2.Price  # target variable
+X2 = df_data2[a]  # features matris
 
-train_X3, val_X3, train_y3, val_y3 = train_test_split(X3, y3, random_state=1)
-# train_test_split(X3, y3, test_size=0.2, random_state=1)
+train_X2, val_X2, train_y2, val_y2 = train_test_split(X2, y2, random_state=1)
+# train_test_split(X2, y2, test_size=0.2, random_state=1)
 # 1,3,4. rows... %80 train, 2,5,6. rows... %20 test
 # random_state=
 
 df_model4 = DecisionTreeRegressor(random_state=1)
-df_model4.fit(train_X3, train_y3)
+df_model4.fit(train_X2, train_y2)
 
-val3_prediction = df_model4.predict(val_X3)
-val3_mae = mean_absolute_error(val3_prediction, val_y3)
-print("Validation MAE: {:,.0f}".format(val3_mae))
+val2_prediction = df_model4.predict(val_X2)
+val2_mae = mean_absolute_error(val2_prediction, val_y2)
+print("Validation MAE: {:,.0f}".format(val2_mae))
 
 
 # Validation MAE: 241,632
@@ -77,7 +77,7 @@ def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
 
 candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
 
-results = {leaf_size: get_mae(leaf_size, train_X3, val_X3, train_y3, val_y3) for leaf_size in candidate_max_leaf_nodes}
+results = {leaf_size: get_mae(leaf_size, train_X2, val_X2, train_y2, val_y2) for leaf_size in candidate_max_leaf_nodes}
 best_tree_size = min(results, key=results.get)
 '''
 Out[19]: 
@@ -93,4 +93,4 @@ Out[20]: 500
 '''
 
 final_model = DecisionTreeRegressor(max_leaf_nodes=best_tree_size, random_state=1)
-final_model.fit(X3, y3)
+final_model.fit(X2, y2)
--- random_forest.py
+++ random_forest.py
@@ -1,8 +1,43 @@
 import  pandas as pd
-from sklearn.metrics import mean_absolute_error
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 
 pd.set_option('display.max_columns',None)
 pd.set_option('display.width', 500)
+
+df_data3 = pd.read_csv("datasets/melb_data.csv")
+
+c= ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+
+y= df_data3.Price
+X= df_data3[c]
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+normal_model= DecisionTreeRegressor(random_state=1)
+normal_model.fit(train_X,train_y)
+val_prediction = normal_model.predict(val_X)
+val_mae = mean_absolute_error(val_prediction,val_y)
+print("Validation MAE when not specifying max_leaf_nodes: {:,.0f}".format(val_mae))
+
+#Validation MAE when not specifying max_leaf_nodes: 241,632
+
+normal_model2 = DecisionTreeRegressor(max_leaf_nodes=100, random_state=1)
+normal_model2.fit(train_X, train_y)
+val_predictions = normal_model2.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE for best value of max_leaf_nodes: {:,.0f}".format(val_mae))
+
+#Validation MAE for best value of max_leaf_nodes: 243,732
+
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+forest_preds = forest_model.predict(val_X)
+forest_model_mae = mean_absolute_error(val_y,forest_preds)
+print("Validation MAE for Random Forest Model: {}".format(forest_model_mae))
+
+#Validation MAE for Random Forest Model: 180544.06532524488
+
 
